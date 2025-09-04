16c9ef1c9fc59578673505b9771ab370e701b61f
Sara Correa <correasara22@gmail.com>
2025-09-03 22:57:54 -0500

    busqueda de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+    
+
+    class Meta:
+        verbose_name = "Mi modelo"
+        verbose_name_plural = "Mis modelos"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+    
+
+    class Meta:
+        verbose_name = "Mi modelo"
+        verbose_name_plural = "Mis modelos"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"pk": self.pk})
+    
--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
-def home (request):
-    #return HttpResponse ('<h1>Welcome to Home page</h1>')
-    #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Sara Correa'})
+def home(request):
+    #return render(request, 'movie/home.html', {'name': 'Sara Correa'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icotains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request, 'movie/home.html', {'searchTerm': searchTerm, 'movies': movies})
 
-def about (request):
-    return HttpResponse('<h1>Welcome to About page</h1>')
-
+def about(request):
+    return render(request, 'movie/about.html')
